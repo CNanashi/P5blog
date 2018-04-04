f3adc98dc6ec4842ea817b0f445b9fffa005fd8e
Nana <Nasunalink@gmail.com>
2018-04-04 11:02:19 +0200

    folders

---
+++ model/db/Hydration.php
@@ -0,0 +1,15 @@
+<?php
+
+abstract class Hydration
+{
+    protected function hydrate(array $data)
+    {
+        foreach ($data as $attribute => $value) {
+            $method = "set" . strtoupper($attribute);
+            if(is_callable([$this, $method])) {
+                $this->$method($value);
+            }
+        }
+    }
+
+}
